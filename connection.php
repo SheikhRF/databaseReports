400cb0456adf25c2f22e0949af6a6d14f4416fd5
SheikhRF <sheikh.rf@oundleschool.org.uk>
2024-11-21 12:33:50 +0000

    hehe

--- connection.php
+++ connection.php
@@ -3,6 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "reports";
+
 $conn = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 echo("connected");
